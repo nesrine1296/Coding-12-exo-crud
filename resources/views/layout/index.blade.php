327093c19994b3d14602c5121b71ab83bb11e358
Nesrine <bouhejbanesrine@gmail.com>
2020-04-10 08:53:45 +0200

    theonlypartiunderstood

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,18 @@
+@yield('templatesections.head')
+
+@yield('templatesections.header')
+
+@yield('templatesections.about us')
+
+@yield('templatesections.services')
+
+@yield('templatesections.portfolio')
+
+@yield('templatesections.testimonials')
+
+@yield('templatesections.team')
+
+@yield('templatesections.contactus')
+
+@yield('templatesections.footer')
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends ('layout.index')
+
+
+
+@section ('head')
+
+@include('layout.templatesections.head')
+
+
+@endsection
+
+
+@section ('aboutus')
+
+@include('layout.templatesections.aboutus')
+
+
+@endsection
